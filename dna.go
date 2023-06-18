aa4738e4909eec64469e9cb3098b287fd3721093
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-06-18 15:58:08 +0100

    all tests passed, updated header & checked error handling

--- dna.go
+++ dna.go
@@ -5,7 +5,7 @@
 
     DNA is a program that identifies a person based on their DNA.
 
-    This is my conversion into Go of the Python program I wrote for Harvardx CS50P problem set 6, with added data validation and error handling. I also replaced the CS50 longest_match function with *. I reused the database and sequence files provided by CS50.
+    This is my conversion into Go of the Python program I wrote for Harvardx CS50P problem set 6, with added data validation and error handling. I also converted and adapted CS50's longest_match function. I reused the database and sequence files provided by CS50.
 
 		Link to my original Python version:
 		https://github.com/laurapg1214/Harvard-CS50-lab-problemsets-Python/tree/main/CS50P-a-pset6-dna
@@ -72,7 +72,7 @@ func main() {
 				strMatches = longestMatch(string(sequence), string(key))
 				personStrCount, err = strconv.Atoi(csvPerson[key])
 				if err != nil {
-					fmt.Println("Error")
+					log.Fatal(err)
 				}
 				// check for match
 				if strMatches == personStrCount {
