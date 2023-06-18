d6776b4f62ff8ce11a1a86716fe277a4bb195d64
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-06-18 11:32:30 +0100

    new func for importing csv to map incomplete

--- dna.go
+++ dna.go
@@ -8,7 +8,7 @@
     This is my conversion into Go of the Python program I wrote for Harvardx CS50P problem set 6, with added data validation and error handling. I also replaced the CS50 longest_match function with *. I reused the database and sequence files provided by CS50.
 
 		Link to my original Python version:
-		https://github.com/laurapg1214/Harvard-CS50-lab-problemsets-Python/tree/main/CS50P-pset6-dna
+		https://github.com/laurapg1214/Harvard-CS50-lab-problemsets-Python/tree/main/CS50P-a-pset6-dna
 
 		Link to the CS50 problem set:
 		https://cs50.harvard.edu/x/2023/psets/6/dna/  */
@@ -19,6 +19,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -42,8 +43,8 @@ func main() {
 		log.Fatalf("Unable to read file: %v", err)
 	}
 
-	// initialize array for names
-	names := [...]string{}
+	// initialize slice for names
+	names := []string{}
 
 	// create map for csv file
 	csvFile, err := os.Open(csvFilename)
@@ -54,24 +55,81 @@ func main() {
 	}
 	defer csvFile.Close()
 
+	// CSVToMap takes a reader and returns an array of maps, using the header row as the keys
+	// adapted from https://gist.github.com/drernie/5684f9def5bee832ebc50cabb46c377a
+
 	// read csv data
+	//csvReader := csv.NewReader(csvFile)
+	//csvReader.FieldsPerRecord = -1 // allow variable number of fields
+	//csvReader.Columns, err = csvReader.ReadHeader()
+	//if err != nil {
+		//fmt.Println(err)
+		//return
+	//}
+
 	csvReader := csv.NewReader(csvFile)
-	csvReader.FieldsPerRecord = -1 // allow variable number of fields
-	csvData, err := csvReader.ReadAll()
+	CSVToMap(csvFile, csvReader)
+
+	//csvData, err := csvReader.ReadAll()
 
 	// error checking
-	if err != nil {
-		panic(err)
-	}
+	//if err != nil {
+		//panic(err)
+	//}
+
+	// TESTING print one record
+	//fmt.Println(csvData[1]["name"])
 
 	// TESTING print csv data
-	for _, row := range csvData {
-		for _, col := range row {
-			fmt.Printf("%s,", col)
-		}
-		fmt.Println()
-		fmt.Println(sequence)
-		fmt.Println(names)
-	}
+	//for _, row := range csvData {
+		//for _, col := range row {
+			//fmt.Printf("%s,", col)
+		//}
+		//fmt.Println()
+	//}
+	fmt.Print(string(sequence))
+	//fmt.Println(names)
 
+	// loop through csv file to append info to names[]
+	//for _, row := range csvData {
+		//for _, col := range row {
+			//names = append(names, col)
+		//}
+	//}
+
+	// initialize slice to store STR counts
+	STRcounts := []int{}
+
+	// TESTING print STRcounts
+	fmt.Println(STRcounts)
+
+	// initialize map for names
+	// STRmap := map[string]int {
+		
+	// }
+
+}
+
+func CSVToMap(csvFile *os.File, csvReader *csv.Reader) []map[string]string {
+	rows := []map[string]string{}
+	var header []string
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if header == nil {
+			header = record
+		} else {
+			csvMap := map[string]string{}
+			for i := range header {
+				csvMap[header[i]] = record[i]
+			}
+			rows = append(rows, csvMap)
+		}
+	}
+	return rows
 }
